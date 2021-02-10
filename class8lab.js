8a56e4d22b8c947fbe8faa394121bb67a4d4409e
Maddie Jones <maddiejones@Maddies-MacBook-Air.local>
2021-02-10 17:02:17 -0600

    Initial commit

---
+++ class8lab.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    var name = document.getElementById(#firstName);
+    var grade = document.getElementById(#grade);
+
+    $("form").validate(
+        {
+
+        }
+    );
+        if (grade > 5) {
+            alert('Information Invalid');
+        }
+        if (grade = 5 <= 8) {
+            alert('You have registered' document.getElementById('#name')
+            'for grade' document.getElementById('#grade') 'camp!');
+        }
+        else {
+            alert("Information Invalid");
+        }
+    function runMyProgram() {
+    }
+return runMyProgram;
+
+
+});
